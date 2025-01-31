4235fa5ae0446cf0b00fc49f51ab71a6f3ed921e
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-01-31 21:53:34 +0300

    Created 'Watermelon' by A2SV Hub

---
+++ 31-Jan-2025/Watermelon 232134.py
@@ -0,0 +1,11 @@
+# Problem: Watermelon - https://codeforces.com/problemset/problem/4/A
+
+testCase = int(input())
+if testCase <=2:
+    print("NO")
+    exit()
+result = testCase-2
+if result%2==0:
+    print("YES")
+else:
+    print("NO")
