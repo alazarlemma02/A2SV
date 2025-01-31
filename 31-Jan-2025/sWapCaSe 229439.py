48392c92076ac33584827a3d4715fcbff9896521
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-01-31 07:37:02 +0300

    Created 'sWapCaSe' by A2SV Hub

---
+++ 31-Jan-2025/sWapCaSe 229439.py
@@ -0,0 +1,15 @@
+# Problem: sWapCaSe - https://www.hackerrank.com/challenges/swap-case/problem?isFullScreen=true
+
+def swap_case(s):
+    res = ''
+    for char in s:
+        if char.islower():
+            res += char.upper()
+        else:
+            res += char.lower()
+    return res
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
