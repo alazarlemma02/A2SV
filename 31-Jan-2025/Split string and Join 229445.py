1480c042e2cb9b7d3ac16ada0f45687039f67f6c
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-01-31 07:39:28 +0300

    Created 'Split string and Join' by A2SV Hub

---
+++ 31-Jan-2025/Split string and Join 229445.py
@@ -0,0 +1,12 @@
+# Problem: Split string and Join - https://www.hackerrank.com/challenges/python-string-split-and-join/problem?isFullScreen=true
+
+
+
+def split_and_join(line):
+    line = line.split(" ")
+    return "-".join(line)
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
