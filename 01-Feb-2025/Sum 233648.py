4abe87841a84136a0b3c57a0b88e81926ae5068e
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-01 19:39:39 +0300

    Created 'Sum' by A2SV Hub

---
+++ 01-Feb-2025/Sum 233648.py
@@ -0,0 +1,14 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+t = input()
+
+for _ in range(int(t)):
+    n = list(map(int, input().split()))
+    if n[1]+n[2]==n[0]:
+        print("YES")
+    elif n[0]+n[2]==n[1]:
+        print("YES")
+    elif n[0]+n[1]==n[2]:
+        print("YES")
+    else:
+        print("NO")
