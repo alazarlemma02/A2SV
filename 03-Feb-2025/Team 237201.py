9f7e2faa0da583748cfe6c9fa7bd336cf3fe085b
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-03 21:04:12 +0300

    Created 'Team' by A2SV Hub

---
+++ 03-Feb-2025/Team 237201.py
@@ -0,0 +1,8 @@
+# Problem: Team - https://codeforces.com/contest/231/problem/A
+
+problem_count = 0
+for _ in range(int(input())):
+    view = map(int, input().split())
+    if sum(view) > 1:
+        problem_count +=1
+print(problem_count)
