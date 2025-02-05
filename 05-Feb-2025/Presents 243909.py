cb614b5c2310e4bbf037a26b72dfd7d255ec0b14
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-05 17:30:07 +0300

    Created 'Presents' by A2SV Hub

---
+++ 05-Feb-2025/Presents 243909.py
@@ -0,0 +1,18 @@
+# Problem: Presents - https://codeforces.com/problemset/problem/136/A
+
+def giftRecievers(n, gifts):
+    friends = []
+    for f in range(n):
+        friends.append([gifts[f], f+1])
+
+    friends.sort()
+
+    ans = []
+    for i in range(len(friends)):
+        ans.append(str(friends[i][1]))
+
+    print(" ".join(ans))
+
+n = int(input())
+gifts = list(map(int, input().split()))
+giftRecievers(n, gifts)
