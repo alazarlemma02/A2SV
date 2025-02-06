b29a523581b07055de887d3ad023b6a96f8138f7
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-06 11:38:00 +0300

    Created 'Escape The Ghosts' by A2SV Hub

---
+++ 06-Feb-2025/Escape The Ghosts 246025.py
@@ -0,0 +1,11 @@
+# Problem: Escape The Ghosts - https://leetcode.com/problems/escape-the-ghosts/
+
+class Solution:
+    def escapeGhosts(self, ghosts: List[List[int]], target: List[int]) -> bool:
+        me = [0,0]
+        my_distance = abs(me[0]-target[0]) + abs(me[1]-target[1])
+        for ghost in ghosts:
+            ghost_distance = abs(ghost[0]-target[0]) + abs(ghost[1]-target[1])
+            if ghost_distance <= my_distance:
+                return False
+        return True     
