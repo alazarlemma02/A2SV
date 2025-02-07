d2624fa743bd88002770db288f3a634735104613
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-07 20:32:18 +0300

    Created 'Majority Element' by A2SV Hub

---
+++ 07-Feb-2025/Majority Element 250490.py
@@ -0,0 +1,14 @@
+# Problem: Majority Element - https://leetcode.com/problems/majority-element/description/
+
+class Solution(object):
+    def majorityElement(self, nums):
+        counts = {}
+        for i in nums:
+            if i in counts:
+                counts[i]+=1
+            else:
+                counts[i]=1
+            if len(nums) // 2 < counts[i]:
+                return i
+                
+        
