3313a87cfb03ab52536b91c4323bfba05a014301
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-08 15:36:05 +0300

    Created 'Same Differences' by A2SV Hub

---
+++ 08-Feb-2025/Same Differences 251401.py
@@ -0,0 +1,16 @@
+# Problem: Same Differences - https://codeforces.com/problemset/problem/1520/D
+
+test_cases = int(input())
+
+for _ in range(test_cases):
+    n = int(input())
+    a = list(map(int, input().split()))
+    count = 0
+    MAP = {}
+    for i in range(n):
+        if a[i]-i in MAP:
+            count += MAP[a[i]-i]
+            MAP[a[i]-i] += 1
+        else:
+            MAP[a[i]-i] = 1
+    print(count)
