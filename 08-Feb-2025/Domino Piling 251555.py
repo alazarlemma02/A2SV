2b6fc0f871fdff86eb3a0b485803e73b908a7be5
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-08 19:22:14 +0300

    Created 'Domino Piling' by A2SV Hub

---
+++ 08-Feb-2025/Domino Piling 251555.py
@@ -0,0 +1,9 @@
+# Problem: Domino Piling - https://codeforces.com/problemset/problem/50/A
+
+def dominoPiling(m, n):
+    larger_area = m * n
+    qube_area = 2 * 1
+    print(larger_area // qube_area)
+
+m, n = list(map(int, input().split()))
+dominoPiling(m, n)
