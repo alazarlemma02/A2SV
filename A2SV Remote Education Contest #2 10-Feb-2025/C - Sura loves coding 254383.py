b013f46ae2993f39873533d300f2b19a8011ba4c
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-10 21:57:12 +0300

    Created 'C - Sura loves coding' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 10-Feb-2025/C - Sura loves coding 254383.py
@@ -0,0 +1,20 @@
+# Problem: C - Sura loves coding - https://codeforces.com/gym/586622/problem/C
+
+def sureLovesCoding(n, s):
+    res = ""
+    count = n
+    for char in s:
+        if count % 2 ==0:
+            res = char + res
+        else:
+            res += char
+        count -=1
+    return res
+
+n = int(input())
+s = list(input())
+
+result = sureLovesCoding(n, s)
+print(result)
+
+
