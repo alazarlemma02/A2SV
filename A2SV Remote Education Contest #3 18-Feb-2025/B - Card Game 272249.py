7aad857b45b3dea303e87505ffef2fc3b814b8d5
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-18 19:28:22 +0300

    Created 'B - Card Game' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 18-Feb-2025/B - Card Game 272249.py
@@ -0,0 +1,19 @@
+# Problem: B - Card Game - https://codeforces.com/gym/588094/problem/B
+
+from collections import defaultdict
+n = int(input())
+
+cards = list(map(int, input().split()))
+
+idx_map = defaultdict(list)
+for c in range(n):
+    idx_map[cards[c]] += [c+1]
+
+cards.sort()
+left, right = 0, n-1
+while left < right:
+    left_idx = idx_map[cards[left]].pop(0)
+    right_idx = idx_map[cards[right]].pop(0)
+    print(f"{left_idx} {right_idx}")
+    left +=1
+    right -=1
