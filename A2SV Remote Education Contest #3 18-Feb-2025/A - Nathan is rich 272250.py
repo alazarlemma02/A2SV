378ee0e19fbc0d53124a69293c90581c00f89af9
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-18 19:28:28 +0300

    Created 'A - Nathan is rich' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 18-Feb-2025/A - Nathan is rich 272250.py
@@ -0,0 +1,14 @@
+# Problem: A - Nathan is rich - https://codeforces.com/gym/588094/problem/A
+
+t = int(input())
+
+for v in range(t):
+    n = int(input())
+    if n <=2:
+        print(n//2)
+    elif n % 4==0:
+        print(n//4)
+    else:
+        r4 = n // 4
+        r2 = (n - r4*4) // 2
+        print(r4 + r2)
