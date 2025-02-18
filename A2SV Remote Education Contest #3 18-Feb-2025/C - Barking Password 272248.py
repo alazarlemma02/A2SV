6c1ecc1d9c1b337e94153aa90dddd92972d04d65
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-18 19:27:58 +0300

    Created 'C - Barking Password' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 18-Feb-2025/C - Barking Password 272248.py
@@ -0,0 +1,30 @@
+# Problem: C - Barking Password - https://codeforces.com/gym/588094/problem/C
+
+password = input()
+n = int(input())
+words = []
+
+for s in range(n):
+    word = input()
+    words.append(word)
+
+if password in words:
+    print("YES")
+    exit()
+
+first_char = False
+for word in words:
+    if word[1] == password[0]:
+        first_char = True
+        break
+
+second_char = False
+for word in words:
+    if word[0] == password[1]:
+        second_char = True
+        break
+
+if first_char and second_char:
+    print("YES")
+else:
+    print("NO")
