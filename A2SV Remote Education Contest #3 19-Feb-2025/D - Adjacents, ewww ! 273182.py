e94fc9f7316f51f6a6f2883785c984fd99142714
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-19 11:29:28 +0300

    Created 'D - Adjacents, ewww !' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 19-Feb-2025/D - Adjacents, ewww ! 273182.py
@@ -0,0 +1,28 @@
+# Problem: D - Adjacents, ewww ! - https://codeforces.com/gym/588094/problem/D
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+
+    if n ==2:
+        print(-1)
+        continue
+    
+    matrix = [[0]*n for _ in range(n)]
+
+    odd = 1
+    for i in range(n):
+        for j in range(n):
+            if odd <= n**2:
+                matrix[i][j] = odd
+                odd += 2
+    even = 2
+    for i in range(n):
+        for j in range(n):
+            if matrix[i][j] == 0:
+                matrix[i][j] = even
+                even += 2
+
+    for row in matrix:
+        print(*row)
