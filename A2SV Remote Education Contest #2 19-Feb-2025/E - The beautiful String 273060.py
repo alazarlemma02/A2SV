a8e108fc963d6d1110da8d0d5b0049d50859da37
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-19 10:30:03 +0300

    Created 'E - The beautiful String' by A2SV Hub

---
+++ A2SV Remote Education Contest #2 19-Feb-2025/E - The beautiful String 273060.py
@@ -0,0 +1,27 @@
+# Problem: E - The beautiful String - https://codeforces.com/gym/586622/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    s = list(input())
+    q = int(input())
+    idx_set = set()
+
+    for b in range(len(s)):
+      if s[b:b+4] == ['1', '1', '0', '0']:
+         idx_set.add(b)
+    for _ in range(q):
+      idx, val = map(int, input().split())
+      idx -= 1
+      if s[idx] == str(val):
+         print("YES" if idx_set else "NO")
+         continue
+      s[idx] = str(val)
+      start = max(idx-3, 0)
+      end = min(len(s)-3, idx+1)
+      for i in range(start, end):
+          if s[i:i+4] == ['1', '1', '0', '0']:
+            idx_set.add(i)
+          else:
+             idx_set.discard(i)
+      print("YES" if idx_set else "NO")
