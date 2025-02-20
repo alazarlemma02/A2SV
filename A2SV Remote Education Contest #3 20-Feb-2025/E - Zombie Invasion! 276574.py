0ba6267873365d87f9383ab8831084b0ae37098c
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-20 12:42:22 +0300

    Created 'E - Zombie Invasion!' by A2SV Hub

---
+++ A2SV Remote Education Contest #3 20-Feb-2025/E - Zombie Invasion! 276574.py
@@ -0,0 +1,24 @@
+# Problem: E - Zombie Invasion! - https://codeforces.com/gym/588094/problem/E
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = list(map(int, input().split()))
+    x = list(map(int, input().split()))
+
+    zombies = sorted(zip(x,a), key= lambda z: abs(z[0]))
+    is_dead = True
+    total_needed_bullets = 0
+
+    for z in range(n):
+        xi, ai = zombies[z]
+
+        total_needed_bullets += ai
+
+        can_shoot = abs(xi)* k
+
+        if total_needed_bullets > can_shoot:
+            is_dead = False
+            break
+    print("YES" if is_dead else "NO")
