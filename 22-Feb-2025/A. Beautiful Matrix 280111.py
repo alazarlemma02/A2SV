0c25cc91be00329405f986ed84e3e73b0331b36c
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-22 09:02:23 +0300

    Created 'A. Beautiful Matrix' by A2SV Hub

---
+++ 22-Feb-2025/A. Beautiful Matrix 280111.py
@@ -0,0 +1,15 @@
+# Problem: A. Beautiful Matrix - https://codeforces.com/problemset/problem/263/A
+
+def beautifulMatrix(matrix):
+    rl, cl = len(matrix), len(matrix[0])
+    rc = cc = rl // 2
+    res = 0
+    for row in range(rl):
+        for col in range(cl):
+            if matrix[row][col] == 1:
+                res = abs(row-rc) + abs(col-cc)
+                return res
+
+
+matrix = [list(map(int, input().split())) for _ in range(5)]
+print(beautifulMatrix(matrix=matrix))
