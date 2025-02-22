2de7ae84821553790d7c4a38c316f6c19e266886
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-22 10:48:40 +0300

    Created 'Set Matrix Zeroes' by A2SV Hub

---
+++ 22-Feb-2025/Set Matrix Zeroes 280147.py
@@ -0,0 +1,26 @@
+# Problem: Set Matrix Zeroes - https://leetcode.com/problems/set-matrix-zeroes/
+
+class Solution:
+    def setZeroes(self, matrix: List[List[int]]) -> None:
+        """     
+        Do not return anything, modify matrix in-place instead.
+        """
+        rl, cl = len(matrix), len(matrix[0])
+        idx_set = set()
+
+        for row in range(rl):
+            for col in range(cl):
+                if matrix[row][col] == 0:
+                    idx_set.add((row,col))
+
+        for row in range(rl):
+            for col in range(cl):
+                if (row,col) in idx_set:
+                    r, c = 0, 0
+                    while r < rl:
+                        matrix[r][col] = 0
+                        r +=1
+                    while c < cl:
+                        matrix[row][c] = 0
+                        c +=1                  
+                   
