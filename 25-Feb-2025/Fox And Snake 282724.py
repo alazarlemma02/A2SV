7ccdcf5c079bf5227ff91ccad29d8b0efb5bba96
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-25 22:12:58 +0300

    Created 'Fox And Snake' by A2SV Hub

---
+++ 25-Feb-2025/Fox And Snake 282724.py
@@ -0,0 +1,21 @@
+# Problem: Fox And Snake - https://codeforces.com/problemset/problem/510/A
+
+rl, cl = map(int, input().split())
+
+left = False
+right = True
+
+for row in range(rl):
+    for col in range(cl):
+        if row % 2 == 0:
+            print("#",end="")
+        else:
+            if col == cl-1 and right:
+                print("#",end="")
+            elif col == 0 and left:
+                print("#",end="")
+            else:
+                print(".",end="")
+    if row % 2 != 0:
+        left, right = right, left
+    print()
