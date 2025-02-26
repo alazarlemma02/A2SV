2b123c06dca8e7016a1f96fdd53307ea89abfebe
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-26 22:58:21 +0300

    Created 'Maximum Sum of an Hourglass - LeetCode' by A2SV Hub

---
+++ 26-Feb-2025/Maximum Sum of an Hourglass - LeetCode 285501.py
@@ -0,0 +1,14 @@
+# Problem: Maximum Sum of an Hourglass - LeetCode - https://leetcode.com/problems/maximum-sum-of-an-hourglass/description/
+
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        max_sum = 0
+        rl,cl = len(grid), len(grid[0])
+        
+        for row in range(rl-2):
+            for col in range(cl-2):
+                curr_sum = sum(grid[row][col:col+3]) + grid[row+1][col+1] + sum(grid[row+2][col:col+3])
+                max_sum = max(max_sum, curr_sum)
+                    
+        return max_sum
+                
