09165d0f81adde8284413a25aa78a2cf3fd8dd7d
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-02-27 20:02:47 +0300

    Created 'Chat Order' by A2SV Hub

---
+++ 27-Feb-2025/Chat Order 286287.py
@@ -0,0 +1,17 @@
+# Problem: Chat Order - https://codeforces.com/problemset/problem/637/B
+
+from collections import OrderedDict
+
+def chatOrder(n):
+    chat_map = OrderedDict()
+
+    for _ in range(n):
+        msgr = input()
+        if msgr in chat_map:
+            del chat_map[msgr]
+        chat_map[msgr] = True
+
+    print("\n".join(reversed(chat_map.keys())))
+
+n = int(input())
+chatOrder(n)
