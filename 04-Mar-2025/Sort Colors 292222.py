c488d55ed3c0c41421b027b540f9c06066e37285
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-04 22:07:19 +0300

    Created 'Sort Colors' by A2SV Hub

---
+++ 04-Mar-2025/Sort Colors 292222.py
@@ -0,0 +1,19 @@
+# Problem: Sort Colors - https://leetcode.com/problems/sort-colors/
+
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        not_swapped = True
+        for i in range(n):
+            not_swapped = True
+            for j in range(n-i-1):
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                    not_swapped = False
+            if not_swapped:
+                break
+                        
+        
