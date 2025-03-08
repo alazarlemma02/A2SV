41f43054fc35f9af40884671b36ffda90ac0acb9
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-08 20:00:26 +0300

    Created 'Third Maximum Number' by A2SV Hub

---
+++ 08-Mar-2025/Third Maximum Number 298065.py
@@ -0,0 +1,13 @@
+# Problem: Third Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution(object):
+    def thirdMax(self, nums):
+        uniq_nums = list(set(nums))
+        uniq_nums.sort(reverse=True)
+
+        if len(uniq_nums)<3:
+            return uniq_nums[0]
+        else:
+            return uniq_nums[2]
+
+        
