23b99f6d33a56fc49d9b28071562e59cbbb1d610
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-09 19:55:06 +0300

    Created 'Reduction Operations to Make the Array Elements Equal - LeetCode' by A2SV Hub

---
+++ 09-Mar-2025/Reduction Operations to Make the Array Elements Equal - LeetCode 298739.py
@@ -0,0 +1,16 @@
+# Problem: Reduction Operations to Make the Array Elements Equal - LeetCode - https://leetcode.com/problems/reduction-operations-to-make-the-array-elements-equal/description/
+
+class Solution:
+    def reductionOperations(self, nums: List[int]) -> int:
+        nums.sort()
+        n = len(nums)
+
+        count = 0
+        for i in range(n-1):
+            if nums[i] != nums[i+1]:
+                count += n - (i+1)
+
+        return count
+        
+
+        
