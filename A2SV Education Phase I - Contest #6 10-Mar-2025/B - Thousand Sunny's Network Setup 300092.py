e221981dd918c766cea9ffdcb322e1865f25b424
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-10 14:32:37 +0300

    Created 'B - Thousand Sunny's Network Setup' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/B - Thousand Sunny's Network Setup 300092.py
@@ -0,0 +1,9 @@
+# Problem: B - Thousand Sunny's Network Setup - https://codeforces.com/gym/594356/problem/B
+
+n, k = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+a.sort(reverse=True)
+
+print(a[k-1])
