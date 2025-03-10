bcaa8766aa13a7c87e16198d1fb1248012f6c95b
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-10 20:35:10 +0300

    Created 'A - Zoro’s Bounty Dilemma' by A2SV Hub

---
+++ A2SV Education Phase I - Contest #6 10-Mar-2025/A - Zoro’s Bounty Dilemma 300621.py
@@ -0,0 +1,15 @@
+# Problem: A - Zoro’s Bounty Dilemma - https://codeforces.com/gym/594356/problem/A
+
+t = int(input())
+
+for _ in range(t):
+    s = list(map(str, input().split()))
+    if ">" in s[0] and "<" in s[0]:
+        print("?")
+    elif ">" in s[0]:
+        print(">")
+    elif "<" in s[0]:
+        print("<")
+    else:
+        print("=")
+
