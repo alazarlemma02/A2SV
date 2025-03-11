b1f08f9df9d6daa711a00d79bb9fc7e54ebf560a
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-11 22:55:40 +0300

    Created 'Number of smaller' by A2SV Hub

---
+++ 11-Mar-2025/Number of smaller 303322.py
@@ -0,0 +1,16 @@
+# Problem: Number of smaller - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/B
+
+n, m = map(int, input().split())
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+res = []
+prev = 0
+
+for num in b:
+    while prev < len(a) and a[prev] < num:
+        prev +=1
+    res.append(prev)
+
+print(" ".join(map(str, res)))
