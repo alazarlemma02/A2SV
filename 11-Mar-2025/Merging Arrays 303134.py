eb608082ca9266f3021a0b14d8698a1f87603c9c
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-11 21:36:14 +0300

    Created 'Merging Arrays' by A2SV Hub

---
+++ 11-Mar-2025/Merging Arrays 303134.py
@@ -0,0 +1,24 @@
+# Problem: Merging Arrays - https://codeforces.com/edu/course/2/lesson/9/1/practice/contest/307092/problem/A
+
+n, m = map(int, input().split())
+
+
+a = list(map(int, input().split()))
+b = list(map(int, input().split()))
+
+ap, bp = 0, 0
+
+res = []
+
+while ap < n and bp < m:
+
+    if a[ap] < b[bp]:
+        res.append(a[ap])
+        ap +=1
+    else:
+        res.append(b[bp])
+        bp +=1
+
+res.extend(a[ap:])
+res.extend(b[bp:])
+print(" ".join(map(str,res)))
