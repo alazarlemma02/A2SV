96480c7544116d157dc1b080678c273a13e4a1d9
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-13 15:14:52 +0300

    Created 'Less or Equal' by A2SV Hub

---
+++ 13-Mar-2025/Less or Equal 304884.py
@@ -0,0 +1,19 @@
+# Problem: Less or Equal - https://codeforces.com/problemset/problem/977/C
+
+def lessrOrEqual(n,k,a):
+    a.sort()
+
+    if k == 0:
+        return a[0]-1 if a[0]>1 else -1
+    kth = a[k-1]
+
+    if k < n  and  a[k]==kth:
+        return -1
+    return kth
+
+
+n, k = map(int, input().split())
+
+a = list(map(int, input().split()))
+
+print(lessrOrEqual(n,k,a))
