e4b2784be8cb22883cf4adaadef5c590abb6fe92
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-14 23:01:16 +0300

    Created 'Books' by A2SV Hub

---
+++ 14-Mar-2025/Books 307495.py
@@ -0,0 +1,21 @@
+# Problem: Books - https://codeforces.com/contest/279/problem/B
+
+def books(n,t,a):
+    left, right = 0, 0
+    max_count = 0
+    curr = 0
+
+    while right < n:
+        curr += a[right]
+        while curr > t:
+            curr -= a[left]
+            left +=1
+        max_count = max(max_count, right-left+1)
+        right +=1
+
+    return max_count
+
+n, t = map(int, input().split())
+a = list(map(int, input().split()))
+
+print(books(n,t,a))
