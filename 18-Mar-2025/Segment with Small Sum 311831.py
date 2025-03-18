bf72185f5e1cf33ceb7795973f4eb4c851acb8d3
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-18 22:31:03 +0300

    Created 'Segment with Small Sum' by A2SV Hub

---
+++ 18-Mar-2025/Segment with Small Sum 311831.py
@@ -0,0 +1,22 @@
+# Problem: Segment with Small Sum - https://codeforces.com/edu/course/2/lesson/9/2/practice/contest/307093/problem/A
+
+n, s = map(int, input().split())
+a = list(map(int, input().split()))
+
+longest_seg = float('-inf')
+curr_seg = 0
+i = 0
+
+for j in range(n):
+
+    curr_seg += a[j]
+
+    while curr_seg > s:
+        prev_sum = curr_seg
+        curr_seg -= a[i]
+        i +=1
+
+    longest_seg = max(longest_seg, j - i + 1)
+
+
+print(longest_seg)
