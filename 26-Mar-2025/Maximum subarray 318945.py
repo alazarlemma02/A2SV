97a39d2ced8c6a0841060163fb8d48dc4229bac8
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-26 18:19:36 +0300

    Created 'Maximum subarray' by A2SV Hub

---
+++ 26-Mar-2025/Maximum subarray 318945.py
@@ -0,0 +1,14 @@
+# Problem: Maximum subarray - https://leetcode.com/problems/maximum-subarray/
+
+class Solution(object):
+    def maxSubArray(self, nums):
+        maximum = nums[0]
+        sum = 0
+        for i in nums:
+            sum += i
+            if sum > maximum:
+                maximum = sum
+            if sum < 0:
+                sum = 0            
+        return maximum
+        
