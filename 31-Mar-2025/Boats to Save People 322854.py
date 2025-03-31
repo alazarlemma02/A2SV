6f09bad96a34dcf8ecbcb83cc89ac6e092001cad
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-03-31 22:40:42 +0300

    Created 'Boats to Save People' by A2SV Hub

---
+++ 31-Mar-2025/Boats to Save People 322854.py
@@ -0,0 +1,27 @@
+# Problem: Boats to Save People - https://leetcode.com/problems/boats-to-save-people/
+
+class Solution:
+    def numRescueBoats(self, people: List[int], limit: int) -> int:
+        n = len(people)
+        min_boats = 0
+        left, right = 0, n-1
+        people.sort()
+        while left <= right:
+            curr = limit-people[right]
+            right -=1
+            min_boats +=1
+            if left <= right and curr >= people[left]:
+                left +=1
+        return min_boats
+
+            
+
+
+            
+            
+
+
+            
+            
+
+        
