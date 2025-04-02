f9e4967a7155df2699ea6254e241173b91bdba27
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-02 09:33:37 +0300

    Created 'Product of Array Except Self' by A2SV Hub

---
+++ 02-Apr-2025/Product of Array Except Self 323742.py
@@ -0,0 +1,15 @@
+# Problem: Product of Array Except Self - https://leetcode.com/problems/product-of-array-except-self/
+
+class Solution(object):
+    def productExceptSelf(self, nums):
+        pp = [1]
+        sp = 1
+        for num in range(1,len(nums)):
+            pp.append(nums[num-1]*pp[-1])
+        curr = len(nums) - 1
+        while curr >= 0:
+            pp[curr] *= sp
+            sp *= nums[curr]
+            curr -= 1
+        return pp  
+        
