9b398cc5acb05c31f9f13fcd3a87224d7fb3d8e4
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-04 11:42:30 +0300

    Created 'Sereja and Dima' by A2SV Hub

---
+++ 04-Apr-2025/Sereja and Dima 325735.py
@@ -0,0 +1,21 @@
+# Problem: Sereja and Dima - https://codeforces.com/problemset/problem/381/A
+
+n = int(input())
+
+cards = list(map(int, input().split()))
+
+s, d = 0, 0
+
+left, right = 0, n-1
+turn = 0
+while left <= right:
+    if turn % 2 == 0:
+        s += max(cards[left], cards[right])
+    else:
+        d += max(cards[left], cards[right])
+    if cards[left] >= cards[right]:
+        left += 1
+    else:
+        right -= 1
+    turn +=1
+print(s, d)
