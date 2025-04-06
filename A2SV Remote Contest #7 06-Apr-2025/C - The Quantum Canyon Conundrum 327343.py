e0a352e2b10e5d31b06303fc648d5fc2555f81a5
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-06 20:27:49 +0300

    Created 'C - The Quantum Canyon Conundrum' by A2SV Hub

---
+++ A2SV Remote Contest #7 06-Apr-2025/C - The Quantum Canyon Conundrum 327343.py
@@ -0,0 +1,30 @@
+# Problem: C - The Quantum Canyon Conundrum - https://codeforces.com/gym/596004/problem/C
+
+def solve(n, a):
+
+    new_a = [a[0]]
+    for i in range(n):
+        if new_a[-1] != a[i]:
+            new_a.append(a[i])
+
+    cnt_canyon = 0
+    k = len(new_a)
+    if k == 1 or k ==2:
+        return "YES"
+    for i in range(k):
+        if i == 0 and new_a[i+1] > new_a[i]:
+            cnt_canyon += 1
+        elif i == k-1 and new_a[i-1] > new_a[i]:
+            cnt_canyon +=1
+        else:
+            if new_a[i-1] > new_a[i] and new_a[i] < new_a[i+1]:
+                cnt_canyon +=1
+    if cnt_canyon != 1:
+        return "NO"
+    return "YES"
+
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input().split()))
+    print(solve(n, a))
