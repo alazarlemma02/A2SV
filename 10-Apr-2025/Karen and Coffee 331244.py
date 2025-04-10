641ea76e377f21cfa5d06c251c3ee409a33626cf
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-10 22:08:31 +0300

    Created 'Karen and Coffee' by A2SV Hub

---
+++ 10-Apr-2025/Karen and Coffee 331244.py
@@ -0,0 +1,30 @@
+# Problem: Karen and Coffee - https://codeforces.com/contest/816/problem/B
+
+from collections import defaultdict
+
+n, k, q = map(int, input().split())
+map1 = defaultdict(int)
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    map1[a] += 1
+    map1[b+1] -= 1
+
+max_temp = 2*10**5+1
+count = [0] * max_temp
+for i in range(1, max_temp):
+    count[i] = count[i-1] + map1[i]
+
+admissible = [0] * max_temp
+for i in range(1, max_temp):
+    admissible[i] = 1 if count[i] >= k else 0
+
+prefix = [0] * max_temp
+for i in range(1, max_temp):
+    prefix[i] = prefix[i-1] + admissible[i]
+
+
+for _ in range(q):
+    left, right = map(int, input().split())
+    print(prefix[right] - prefix[left-1])
+
