9fc3d14d5164ee3acc06ca170b5a48a7a4a28345
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-11 09:44:32 +0300

    Created 'Merge Strings Alternately' by A2SV Hub

---
+++ 11-Apr-2025/Merge Strings Alternately 331483.py
@@ -0,0 +1,20 @@
+# Problem: Merge Strings Alternately - https://leetcode.com/problems/merge-strings-alternately/
+
+class Solution(object):
+    def mergeAlternately(self, word1, word2):
+        result = ""
+        len1 = len(word1)
+        len2 = len(word2)
+        length = len1 + len2
+        for i in range(length):
+            if len1 > 0:
+                result += word1[-1 * len1]
+                len1-=1
+            if len2 > 0:
+                result += word2[-1 * len2]
+                len2 -=1
+        return result
+        
+        
+        
+        
