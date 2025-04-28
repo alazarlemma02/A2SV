827e75b37525e5ae06411d856b7237f70250d386
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-04-28 22:11:53 +0300

    Created 'D - Electric Mayhem' by A2SV Hub

---
+++ A2SV Contest #11 28-Apr-2025/D - Electric Mayhem 344282.py
@@ -0,0 +1,21 @@
+# Problem: D - Electric Mayhem - https://codeforces.com/gym/605795/problem/D
+
+def solve(chars):
+    st = []
+    st.append(chars[0])
+
+    if (len(chars)%2):
+        return "No"
+    
+    for c in range(1, len(chars)):
+        if not st or chars[c] != st[-1]:
+            st.append(chars[c])
+        else:
+            st.pop()
+
+    if not st:
+        return "Yes"
+    return "No"
+
+chars = str(input())
+print(solve(chars))
