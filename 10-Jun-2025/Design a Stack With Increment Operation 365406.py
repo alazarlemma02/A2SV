c604f40beecaa94696600f447b18f3d31a8999fc
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-06-10 22:21:11 +0300

    Created 'Design a Stack With Increment Operation' by A2SV Hub

---
+++ 10-Jun-2025/Design a Stack With Increment Operation 365406.py
@@ -0,0 +1,28 @@
+# Problem: Design a Stack With Increment Operation - https://leetcode.com/problems/design-a-stack-with-increment-operation/
+
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.maxSize = maxSize      
+
+    def push(self, x: int) -> None:
+        if len(self.stack) < self.maxSize:
+            self.stack.append(x)      
+
+    def pop(self) -> int:
+        if not self.stack:
+            return -1
+        return self.stack.pop()         
+
+    def increment(self, k: int, val: int) -> None:
+        limit = min(k, len(self.stack))
+        for i in range(limit):
+            self.stack[i] += val        
+
+
+# Your CustomStack object will be instantiated and called as such:
+# obj = CustomStack(maxSize)
+# obj.push(x)
+# param_2 = obj.pop()
+# obj.increment(k,val)
