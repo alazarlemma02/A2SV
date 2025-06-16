9cd5357a2440e65eea075d1acba5a65d0f9858aa
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-06-16 03:38:10 +0300

    Created 'Regular Graph' by A2SV Hub

---
+++ 16-Jun-2025/Regular Graph 367111.py
@@ -0,0 +1,16 @@
+# Problem: Regular Graph - https://basecamp.eolymp.com/en/problems/5076
+
+# example below, replace it with your solution
+def solve(n, m):
+    degrees = [0]*(n)
+    for _ in range(m):
+        a, b = map(int, input().split())
+        degrees[a-1] +=1
+        degrees[b-1] +=1
+    check = len(set(degrees)) == 1
+    if check:
+        return "YES"
+    return "NO"
+
+n, m = map(int, input().split())
+print(solve(n, m))
