f7a265c058427cba3de4e4936f71430a5ef585bd
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-06-20 21:43:19 +0300

    Created 'Course Schedule' by A2SV Hub

---
+++ 20-Jun-2025/Course Schedule 368666.py
@@ -0,0 +1,29 @@
+# Problem: Course Schedule - https://leetcode.com/problems/course-schedule/
+
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        graph = defaultdict(list)
+
+        for a, b in prerequisites:
+            graph[b].append(a)
+
+        def dfs(node):
+            res = True
+
+            visited[node] = 2
+
+            for ngh in graph[node]:
+                if visited[ngh] == 2:
+                    return False
+                if not visited[ngh]:
+                    res = res and dfs(ngh)
+
+            visited[node] = 1
+            return res
+            
+        visited = [0]*numCourses
+        res = True
+        for i in range(numCourses):
+            if not visited[i]:
+                res = res and dfs(i)
+        return res
