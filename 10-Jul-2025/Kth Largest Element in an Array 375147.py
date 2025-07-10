4e8b7cfa0bea282547fe455d029d1eadc0f5fd59
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-07-10 20:59:31 +0300

    Created 'Kth Largest Element in an Array' by A2SV Hub

---
+++ 10-Jul-2025/Kth Largest Element in an Array 375147.py
@@ -0,0 +1,10 @@
+# Problem: Kth Largest Element in an Array - https://leetcode.com/problems/kth-largest-element-in-an-array/description/
+
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        heap = []
+        for num in nums:
+            heapq.heappush(heap, num)
+            if len(heap) > k:
+                heapq.heappop(heap)
+        return heap[0]
