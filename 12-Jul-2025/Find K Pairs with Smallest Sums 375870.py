43a8ab63aa2076f5a1789ec78c0c2e2ca875d151
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-07-12 23:40:51 +0300

    Created 'Find K Pairs with Smallest Sums' by A2SV Hub

---
+++ 12-Jul-2025/Find K Pairs with Smallest Sums 375870.py
@@ -0,0 +1,24 @@
+# Problem: Find K Pairs with Smallest Sums - https://leetcode.com/problems/find-k-pairs-with-smallest-sums/description/
+
+class Solution:
+    def kSmallestPairs(self, nums1: List[int], nums2: List[int], k: int) -> List[List[int]]:
+        if not nums1 or not nums2 or not k:
+            return []
+        res, heap, visited = [], [], set()
+        heappush(heap, (nums1[0]+nums2[0], 0, 0))
+        visited.add((0, 0))
+
+        while heap and k:
+            total, i, j = heappop(heap)
+            res.append([nums1[i], nums2[j]])
+
+            if i+1 < len(nums1) and (i+1, j) not in visited:
+                heappush(heap, (nums1[i+1]+nums2[j], i+1, j))
+                visited.add((i+1, j))
+
+            if j+1 < len(nums2) and (i, j+1) not in visited:
+                heappush(heap, (nums1[i]+nums2[j+1], i, j+1))
+                visited.add((i, j+1))
+            k -=1
+        return res
+        
