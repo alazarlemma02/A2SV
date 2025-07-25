d1bdd817fe4354206b506a15165449d1766f2b5a
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-07-25 21:59:40 +0300

    Created 'Find All Possible Recipes From Given Supplies' by A2SV Hub

---
+++ 25-Jul-2025/Find All Possible Recipes From Given Supplies 379374.py
@@ -0,0 +1,29 @@
+# Problem: Find All Possible Recipes From Given Supplies - https://leetcode.com/problems/find-all-possible-recipes-from-given-supplies/
+
+class Solution:
+    def findAllRecipes(self, recipes: List[str], ingredients: List[List[str]], supplies: List[str]) -> List[str]:
+        supply_exist = defaultdict(bool)
+        recipe_map = defaultdict(int)
+        for supply in supplies:
+            supply_exist[supply] = True
+        for idx, recipe in enumerate(recipes):
+            recipe_map[recipe] = idx
+
+        def dfs(recipe):
+            if recipe in supply_exist:
+                return supply_exist[recipe]
+            if recipe not in recipe_map:
+                return False
+
+            supply_exist[recipe] = False
+            for ngh in ingredients[recipe_map[recipe]]:
+                if not dfs(ngh):
+                    return False
+            supply_exist[recipe] = True
+            return True
+
+        res = []
+        for recipe in recipes:
+            if dfs(recipe):
+                res.append(recipe)
+        return res
