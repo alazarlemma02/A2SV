c7e1dc84e93be0c7f8fff17acc572af086ae50eb
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-07-30 21:06:57 +0300

    Created 'Power of Three' by A2SV Hub

---
+++ 30-Jul-2025/Power of Three 380617.py
@@ -0,0 +1,12 @@
+# Problem: Power of Three - https://leetcode.com/problems/power-of-three/
+
+class Solution(object):
+    def isPowerOfThree(self, n):
+        if n <= 0:
+            return False
+        else:
+            val = n
+            while val % 3 == 0:
+                val /= 3
+            return True if val==1 else False
+        
