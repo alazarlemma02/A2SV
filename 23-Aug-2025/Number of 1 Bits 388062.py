32ef3259b67cb1f8424df81238d67f49420e4787
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-08-23 18:06:43 +0300

    Created 'Number of 1 Bits' by A2SV Hub

---
+++ 23-Aug-2025/Number of 1 Bits 388062.py
@@ -0,0 +1,10 @@
+# Problem: Number of 1 Bits - https://leetcode.com/problems/number-of-1-bits/description/
+
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n != 0:
+            n = n & n-1
+            count +=1
+        return count
+        
