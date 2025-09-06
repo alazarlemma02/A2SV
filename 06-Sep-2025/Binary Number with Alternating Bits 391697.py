557683f2b5ac79f845d41b64c60eb1f9290400e6
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-09-06 22:57:49 +0300

    Created 'Binary Number with Alternating Bits' by A2SV Hub

---
+++ 06-Sep-2025/Binary Number with Alternating Bits 391697.py
@@ -0,0 +1,14 @@
+# Problem: Binary Number with Alternating Bits - https://leetcode.com/problems/binary-number-with-alternating-bits/
+
+class Solution:
+    def hasAlternatingBits(self, n: int) -> bool:
+        prev = n & 1
+        n >>= 1
+        while n != 0:
+            curr = n & 1
+            if prev == curr:
+                return False
+            prev = curr
+            n >>=1
+        return True
+        
