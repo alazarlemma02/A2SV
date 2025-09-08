5f752d55f4a25a388b4f72fc3c9ab450dbc8a452
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-09-08 22:32:44 +0300

    Created 'Find All Duplicates in an Array' by A2SV Hub

---
+++ 08-Sep-2025/Find All Duplicates in an Array 392019.py
@@ -0,0 +1,6 @@
+# Problem: Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+class Solution:
+    def findDuplicates(self, nums: List[int]) -> List[int]:
+        nums = Counter(nums)
+        return [num for num in nums if nums[num] == 2]
