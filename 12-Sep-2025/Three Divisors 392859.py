c56448c83166028443b575497915912f1a241cc7
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-09-12 22:49:01 +0300

    Created 'Three Divisors' by A2SV Hub

---
+++ 12-Sep-2025/Three Divisors 392859.py
@@ -0,0 +1,11 @@
+# Problem: Three Divisors - https://leetcode.com/problems/three-divisors/description/?envType=problem-list-v2&envId=number-theory
+
+class Solution:
+    def isThree(self, n: int) -> bool:
+        cnt = 0
+        for i in range(1, n+1):
+            if n % i == 0:
+                cnt += 1
+        if cnt != 3:
+            return False
+        return True
