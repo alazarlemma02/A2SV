004844e94c7224d0d32c97c1b0ee691f05ee3895
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-09-15 16:31:40 +0300

    Created 'Almost Prime' by A2SV Hub

---
+++ 15-Sep-2025/Almost Prime 393277.py
@@ -0,0 +1,19 @@
+# Problem: Almost Prime - https://codeforces.com/problemset/problem/26/A
+
+num = int(input())
+cnt = 0
+
+for i in range(2, num + 1):
+    prime_divisors = 0
+    for j in range(2, i + 1):
+        if i % j == 0:
+            is_prime = True
+            for k in range(2, int(j ** 0.5) + 1):
+                if j % k == 0:
+                    is_prime = False
+                    break
+            if is_prime:
+                prime_divisors += 1
+    if prime_divisors == 2:
+        cnt += 1
+print(cnt)
