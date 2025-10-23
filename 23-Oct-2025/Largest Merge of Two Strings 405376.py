d5c38b4b2b0b1dd5b4dbe70a0747e206f073bdce
Alazar Lemma <112463751+alazarlemma02@users.noreply.github.com>
2025-10-23 21:56:15 +0300

    Created 'Largest Merge of Two Strings' by A2SV Hub

---
+++ 23-Oct-2025/Largest Merge of Two Strings 405376.py
@@ -0,0 +1,21 @@
+# Problem: Largest Merge of Two Strings - https://leetcode.com/problems/largest-merge-of-two-strings/
+
+class Solution:
+    def largestMerge(self, word1: str, word2: str) -> str:
+        merged = []
+        
+        while word1 and word2:
+            if word1 > word2:
+                merged.append(word1[0])
+                word1 = word1[1:]
+            else:
+                merged.append(word2[0])
+                word2 = word2[1:]
+        merged.append(word1)
+        merged.append(word2)
+
+        return "".join(merged)
+
+
+
+                    
